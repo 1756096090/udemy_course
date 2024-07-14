6315e0185dbabcf26bdbf5dbbd046ddb37633d09
1756096090 <84259949+1756096090@users.noreply.github.com>
2024-07-13 21:31:46 -0500

    new: adds

---
+++ Assets/Game/Scrpts/LookY.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookY : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity = 1f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float _mouseY = Input.GetAxis("Mouse Y");
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.x -= _mouseY;
+        transform.localEulerAngles = newRotation;
+    }
+}
